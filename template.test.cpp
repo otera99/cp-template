fe4dff9003afc8993eab9c908cf52985c14e8375
Tatsuya Terao <sirokurocat1999@gmail.com>
2023-11-30 04:09:57 +0900

    modify temlete.cpp

--- template.test.cpp
+++ template.test.cpp
@@ -4,18 +4,24 @@ rev_rep(i, 10) {
 }
 assert((ans == vc<int>{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}));
 out(ans);
+
 ans.clear();
-rev_rep(i, 10, 5) {
+rev_rep(i, 5, 10) {
     ans.eb(i);
 }
+assert((ans == vc<int>{9, 8, 7, 6, 5}));
 out(ans);
+
 ans.clear();
 rev_rep1(i, 10) {
     ans.eb(i);
 }
+assert((ans == vc<int>{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}));
 out(ans);
+
 ans.clear();
-rev_rep1(i, 10, 5) {
+rev_rep1(i, 5, 10) {
     ans.eb(i);
 }
+assert((ans == vc<int>{10, 9, 8, 7, 6, 5}));
 out(ans);
